bb70f32d60b93437050416510054021c3b909a42
ediga-divya <edigadivya2610@gmail.com>
2026-04-02 19:28:01 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -72,6 +72,3 @@ def serve_yaml():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("server:app", host="0.0.0.0", port=7860, reload=False)
-def main():
-    import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=7860, reload=False)
